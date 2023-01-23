883c5a9a3f6e90da11bff0ab975260f5d03a49e8
Steven Murawski <stmuraws@microsoft.com>
2023-01-23 17:58:36 -0600

    week 2 day 1
    Signed-off-by: Steven Murawski <stmuraws@microsoft.com>

---
+++ cnny23-week2-day1.ps1
@@ -0,0 +1,14 @@
+## Setup Environment
+az deployment sub create --template-file ./deploy/main.bicep --location eastus --parameters 'resourceGroup=cnny-week2'
+$AcrName = az deployment sub show --name main --query 'properties.outputs.acr_name.value' -o tsv
+$AksName = az deployment sub show --name main --query 'properties.outputs.aks_name.value' -o tsv
+$ResourceGroup = az deployment sub show --name main --query 'properties.outputs.resource_group_name.value' -o tsv
+az aks get-credentials --resource-group $ResourceGroup --name $AksName
+
+## Build App Container
+az acr build --registry $AcrName --% --image cnny2023/azure-voting-app-rust:{{.Run.ID}} .
+$BuildTag = az acr repository show-tags `
+                              --name $AcrName `
+                              --repository cnny2023/azure-voting-app-rust `
+                              --orderby time_desc `
+                              --query '[0]' -o tsv
